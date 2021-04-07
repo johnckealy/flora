9082979cafed5ac79680e7b013d95a584a1a5183
johnckealy <johnckealy@gmail.com>
2021-04-07 22:05:06 +0100

    add selenium tests

--- api/tests/integration_tests/test_authentication.py
+++ api/tests/integration_tests/test_authentication.py
@@ -8,7 +8,7 @@ class AuthTest(LiveServerTestCase):
     def setUp(self) -> None:
         options = Options()
         options.binary_location = '/usr/bin/brave-browser'
-        # options.headless = True
+        options.headless = True
         options.add_argument('ignore-certificate-errors')
         self.selenium = webdriver.Chrome(options = options)
 
@@ -16,8 +16,9 @@ class AuthTest(LiveServerTestCase):
 
 
     def test_login_page(self):
+        # self.selenium.get('https://flora.johnkealy.com/')
         self.selenium.get('https://127.0.0.1:8080/')
         login_btn = self.selenium.find_element_by_id('login-button')
         login_btn.click()
-        self.assertIn ("Don't have an account", self.selenium.page_source)
+        self.assertIn("Don't have an account", self.selenium.page_source)
         self.selenium.close()
