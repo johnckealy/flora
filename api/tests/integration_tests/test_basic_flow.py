58de95a56967d74f66f19b02ca357f8d925bd2c2
johnckealy <johnckealy@gmail.com>
2021-04-09 00:56:11 +0100

    GA1

--- api/tests/integration_tests/test_basic_flow.py
+++ api/tests/integration_tests/test_basic_flow.py
@@ -46,7 +46,7 @@ class AuthTest(LiveServerTestCase):
         h6_element = WebDriverWait(self.selenium, 30).until(
             EC.presence_of_element_located((By.TAG_NAME, "h6")))
 
-        self.selenium.implicitly_wait(5)
+        self.selenium.implicitly_wait(10)
         self.assertIn("Hi Guest", h6_element.text)
 
         # create a new device entry
