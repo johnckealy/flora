5a00f2780aa643ddfc8b189fd5def35e57ccba23
johnckealy <johnckealy@gmail.com>
2021-04-08 01:13:40 +0100

    GA19

--- api/tests/integration_tests/test_authentication.py
+++ api/tests/integration_tests/test_authentication.py
@@ -19,7 +19,7 @@ class AuthTest(LiveServerTestCase):
 
 
     def test_login_page(self):
-        self.selenium.get(os.environ.get('ORIGIN_URL'))
+        self.selenium.get(self.base_url, 'https://flora.johnkealy.com')
         login_btn = self.selenium.find_element_by_id('login-button')
         login_btn.click()
         self.assertIn("Don\'t have an account", self.selenium.page_source)
