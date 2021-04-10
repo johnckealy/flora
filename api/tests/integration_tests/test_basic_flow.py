b63f67d03f7e79323b765c32059ae3de93a5dfd5
johnckealy <johnckealy@gmail.com>
2021-04-10 17:28:57 +0100

    GA3

--- api/tests/integration_tests/test_basic_flow.py
+++ api/tests/integration_tests/test_basic_flow.py
@@ -56,6 +56,7 @@ class AuthTest(LiveServerTestCase):
         expansion_element = WebDriverWait(self.selenium, 30).until(
             EC.presence_of_element_located((By.CLASS_NAME, "q-item__label")))
 
+        self.selenium.implicitly_wait(20)
         expansion_element.click()
 
         radio_elements = WebDriverWait(self.selenium, 30).until(
